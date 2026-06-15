052efdc883612fd4d3e0e051c0ce19d3d29b3084
nastiashkm24-del <nastia.shkm24@gmail.com>
2026-06-15 18:35:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector('.burger-menu');
+const nav = document.querySelector('.nav');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+
+    document.body.style.overflow =
+        nav.classList.contains('active')
+            ? 'hidden'
+            : '';
+});
